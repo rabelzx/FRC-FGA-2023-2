bb1d3fc0dfd2e70c2059bbbda72b51232b7592b2
eric <eric.rabelo21@gmail.com>
2023-12-17 17:54:44 -0300

    arrumando esta merda

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.secret_key = 'your secret key'
   
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'tigreazul21'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'videoChatApp'
 
 rooms = {}  # Dictionary to store users and messages in each room
